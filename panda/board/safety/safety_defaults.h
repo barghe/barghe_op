8b22749ae4bee831532e8f4f706953ab5964ccaf
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-09-25 22:42:31 +0300

    PUF+F

--- panda/board/safety/safety_defaults.h
+++ panda/board/safety/safety_defaults.h
@@ -1,7 +1,33 @@
 bool HKG_forwarding_enabled = 1;
+int HKG_MDPS12_checksum = -1;
+int HKG_MDPS12_cnt = 0;   
+int HKG_last_StrColT = 0;
 
 void default_rx_hook(CAN_FIFOMailBox_TypeDef *to_push) {
-  UNUSED(to_push);
+  int bus = GET_BUS(to_push);
+  int addr = GET_ADDR(to_push);
+  
+  if (addr == 593) {
+    if (MDPS12_checksum == -1) {
+      int New_Chksum2 = 0;
+      uint8_t dat[8];
+      for (int i=0; i<8; i++) {
+        dat[i] = GET_BYTE(to_push, i);
+      }
+      int Chksum2 = dat[3];
+      dat[3] = 0;
+      for (int i=0; i<8; i++) {
+        New_Chksum2 += dat[i];
+      }
+      New_Chksum2 %= 256;
+      if (Chksum2 == New_Chksum2) {
+        MDPS12_checksum = 0;
+      }
+      else {
+        MDPS12_checksum = 1;
+      }
+    }
+  }
 }
 
 int default_ign_hook(void) {
@@ -27,7 +53,7 @@ static int nooutput_tx_lin_hook(int lin_num, uint8_t *data, int len) {
   return false;
 }
 
-static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
+ static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   int addr = GET_ADDR(to_fwd);
   int bus_fwd = -1;
 
@@ -37,6 +63,70 @@ static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
 
   if (HKG_forwarding_enabled) {
     if (bus_num == 0) {
+      if (addr == 593) {
+        uint8_t dat[8];
+        int New_Chksum2 = 0;
+        for (int i=0; i<8; i++) {
+          dat[i] = GET_BYTE(to_fwd, i);
+        }
+        if (MDPS12_cnt > 330) {
+          int StrColTq = dat[0] | (dat[1] & 0x7) << 8;
+          int OutTq = dat[6] >> 4 | dat[7] << 4;
+          if (MDPS12_cnt == 331) {
+            StrColTq -= 164;
+          }
+          else {
+            StrColTq = last_StrColT + 34;
+          }
+          OutTq = 2058;
+
+          dat[0] = StrColTq & 0xFF;
+          dat[1] &= 0xF8;
+          dat[1] |= StrColTq >> 8;
+          dat[6] &= 0xF;
+          dat[6] |= (OutTq & 0xF) << 4;
+          dat[7] = OutTq >> 4;
+            
+
+          to_fwd->RDLR &= 0xFFF800;
+          to_fwd->RDLR |= StrColTq;
+          to_fwd->RDHR &= 0xFFFFF;
+          to_fwd->RDHR |= OutTq << 20;
+          last_StrColT = StrColTq;
+
+          dat[3] = 0;
+          if (MDPS12_checksum == 0) { 
+            for (int i=0; i<8; i++) {
+              New_Chksum2 += dat[i];
+            }
+            New_Chksum2 %= 256;
+          }
+          else if (MDPS12_checksum == 1) { //we need CRC8 checksum
+            uint8_t crc = 0xFF;
+            uint8_t poly = 0x1D;
+            int i, j;
+            for (i=0; i<8; i++){
+              if (i!=3){ //don't include CRC byte
+                crc ^= dat[i];
+                for (j=0; j<8; j++) {
+                  if ((crc & 0x80) != 0U) {
+                    crc = (crc << 1) ^ poly;
+                  } else
+                  {
+                  crc <<= 1;
+                  }
+                }
+              }
+            }
+            crc ^= 0xFF;
+            crc %= 256;
+            New_Chksum2 = crc;
+          }
+          to_fwd->RDLR |= New_Chksum2 << 24;
+          MDPS12_cnt += 1;
+          MDPS12_cnt %= 345;
+        }
+      }
       bus_fwd = 2;
     }
     if (bus_num == 2) {
