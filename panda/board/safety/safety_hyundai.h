8aa107c5cb463785b287db0fd216bad69dfbc4d9
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-09-26 07:02:59 +0300

    forward mdps

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -141,21 +141,40 @@ static int hyundai_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
   // forward cam to ccan and viceversa, except lkas cmd
   if (!hyundai_camera_detected) {
     if (bus_num == 0) {
-      bus_fwd = hyundai_camera_bus;
+      int addr = GET_ADDR(to_fwd);
+      if (addr != 1265) {
+        bus_fwd = hyundai_camera_bus + 10;
+        } else {
+          if (!OP_LKAS_live) {
+            bus_fwd = hyundai_camera_bus + 10;
+          } else {
+              bus_fwd = hyundai_camera_bus;
+	     	  }
+       }
     }
     if (bus_num == hyundai_camera_bus) {
       int addr = GET_ADDR(to_fwd);
       if (addr != 832) {
-        bus_fwd = 0;
+        bus_fwd = 10;
       }
       else if (!OP_LKAS_live) {
         hyundai_LKAS_forwarded = 1;
-        bus_fwd = 0;
+        bus_fwd = 10;
       }
       else {
         OP_LKAS_live -= 1;
       }
     }
+  }
+  if (bus_num == 1) {
+    int addr = GET_ADDR(to_fwd);
+    //if (addr != 593) {
+      bus_fwd = 20;
+    //} else if (!OP_LKAS_live) {
+        //bus_fwd = 20;
+    //} else {
+      //bus_fwd = 0;
+	  //}
   }
   return bus_fwd;
 }
