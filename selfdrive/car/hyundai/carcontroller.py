55fb9d2c306f4329b233186c69e7d836154a5d58
xx979xx <40252818+xx979xx@users.noreply.github.com>
2019-10-11 22:45:02 +0300

    warning fix

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -28,8 +28,8 @@ def process_hud_alert(enabled, fingerprint, visual_alert, left_line,
 
   # initialize to no line visible
   lane_visible = 1
-  if left_line and right_line:
-    if enabled:
+  if left_line and right_line or hud_alert:
+    if enabled or hud_alert:
       lane_visible = 3
     else:
       lane_visible = 4
