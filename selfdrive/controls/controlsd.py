aef0dfd1198511b345fe0e575b93bb1e5aeba3f2
neokii <neokii@gmail.com>
2020-12-19 22:30:59 +0900

    remove no gps event

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -224,10 +224,10 @@ class Controls:
     if not self.sm['liveLocationKalman'].sensorsOK and not NOSENSOR:
       if self.sm.frame > 5 / DT_CTRL:  # Give locationd some time to receive all the inputs
         self.events.add(EventName.sensorDataInvalid)
-    if not self.sm['liveLocationKalman'].gpsOK and (self.distance_traveled > 1000):
-      # Not show in first 1 km to allow for driving out of garage. This event shows after 5 minutes
-      if not (SIMULATION or NOSENSOR):  # TODO: send GPS in carla
-        self.events.add(EventName.noGps)
+    #if not self.sm['liveLocationKalman'].gpsOK and (self.distance_traveled > 1000):
+    #  # Not show in first 1 km to allow for driving out of garage. This event shows after 5 minutes
+    #  if not (SIMULATION or NOSENSOR):  # TODO: send GPS in carla
+    #    self.events.add(EventName.noGps)
     if not self.sm['pathPlan'].paramsValid:
       self.events.add(EventName.vehicleModelInvalid)
     if not self.sm['liveLocationKalman'].posenetOK:
