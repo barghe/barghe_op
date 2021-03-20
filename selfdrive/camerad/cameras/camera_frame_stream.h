13e3cfc09a698462a858b68a18ce494c30c9a95b
barghe <64267486@naver.com>
2021-03-20 12:41:50 +0900

    openpilot 0.8.2 release

---
+++ selfdrive/camerad/cameras/camera_frame_stream.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <stdbool.h>
+
+#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
+#ifdef __APPLE__
+#include <OpenCL/cl.h>
+#else
+#include <CL/cl.h>
+#endif
+
+#include "camera_common.h"
+
+#define FRAME_BUF_COUNT 16
+
+typedef struct CameraState {
+  int camera_id;
+  int camera_num;
+  CameraInfo ci;
+
+  int fps;
+  float digital_gain;
+  float cur_gain_frac;
+
+  CameraBuf buf;
+} CameraState;
+
+typedef struct MultiCameraState {
+  int ispif_fd;
+
+  CameraState road_cam;
+  CameraState driver_cam;
+
+  SubMaster *sm;
+  PubMaster *pm;
+} MultiCameraState;
+
+void cameras_init(VisionIpcServer * v, MultiCameraState *s, cl_device_id device_id, cl_context ctx);
+void cameras_open(MultiCameraState *s);
+void cameras_run(MultiCameraState *s);
+void cameras_close(MultiCameraState *s);
+void camera_autoexposure(CameraState *s, float grey_frac);
---
+++ selfdrive/modeld/dmonitoringmodeld.cc
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <cassert>
+#include <sys/resource.h>
+
+#include "visionbuf.h"
+#include "visionipc_client.h"
+#include "common/swaglog.h"
+#include "common/util.h"
+
+#include "models/dmonitoring.h"
+
+#ifndef PATH_MAX
+#include <linux/limits.h>
+#endif
+
+
+ExitHandler do_exit;
+
+int main(int argc, char **argv) {
+  setpriority(PRIO_PROCESS, 0, -15);
+
+  PubMaster pm({"driverState"});
+
+  // init the models
+  DMonitoringModelState dmonitoringmodel;
+  dmonitoring_init(&dmonitoringmodel);
+
+  VisionIpcClient vipc_client = VisionIpcClient("camerad", VISION_STREAM_YUV_FRONT, true);
+  while (!do_exit){
+    if (!vipc_client.connect(false)){
+      util::sleep_for(100);
+      continue;
+    }
+    break;
+  }
+
+  while (!do_exit) {
+    LOGW("connected with buffer size: %d", vipc_client.buffers[0].len);
+
+    double last = 0;
+    while (!do_exit) {
+      VisionIpcBufExtra extra = {0};
+      VisionBuf *buf = vipc_client.recv(&extra);
+      if (buf == nullptr){
+        continue;
+      }
+
+      double t1 = millis_since_boot();
+      DMonitoringResult res = dmonitoring_eval_frame(&dmonitoringmodel, buf->addr, buf->width, buf->height);
+      double t2 = millis_since_boot();
+
+      // send dm packet
+      dmonitoring_publish(pm, extra.frame_id, res, (t2-t1)/1000.0, dmonitoringmodel.output);
+
+      LOGD("dmonitoring process: %.2fms, from last %.2fms", t2-t1, t1-last);
+      last = t1;
+    }
+  }
+
+  dmonitoring_free(&dmonitoringmodel);
+
+  return 0;
+}
