2c90f4d41504a1a65c29d77d09670895f99ae599
neokii <neokii@gmail.com>
2022-11-26 10:05:35 +0900

    increase delta up

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -17,7 +17,7 @@ class CarControllerParams:
   ACCEL_MAX = 2.0 # m/s
 
   def __init__(self, CP):
-    self.STEER_DELTA_UP = 3
+    self.STEER_DELTA_UP = 4
     self.STEER_DELTA_DOWN = 7
     self.STEER_DRIVER_ALLOWANCE = 50
     self.STEER_DRIVER_MULTIPLIER = 2
