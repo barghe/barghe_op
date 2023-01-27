84064a23300865b40aeb8f1d8fa3f44be58f3ce4
neokii <neokii@gmail.com>
2023-01-27 13:08:05 +0900

    merge changes

--- panda/board/safety/safety_hyundai.h
+++ panda/board/safety/safety_hyundai.h
@@ -4,9 +4,9 @@
   .max_steer = (steer), \
   .max_rate_up = (rate_up), \
   .max_rate_down = (rate_down), \
-  .max_rt_delta = 200, \
+  .max_rt_delta = 120, \
   .max_rt_interval = 250000, \
-  .driver_torque_allowance = 70, \
+  .driver_torque_allowance = 50, \
   .driver_torque_factor = 2, \
   .type = TorqueDriverLimited, \
    /* the EPS faults when the steering angle is above a certain threshold for too long. to prevent this, */ \
