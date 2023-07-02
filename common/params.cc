970abde687f03417af6fc617396c145f40d21e92
neokii <neokii@gmail.com>
2023-07-02 16:52:10 +0900

    fix

--- common/params.cc
+++ common/params.cc
@@ -203,6 +203,7 @@ std::unordered_map<std::string, uint32_t> keys = {
     {"UpdaterTargetBranch", CLEAR_ON_MANAGER_START},
     {"Version", PERSISTENT},
     {"VisionRadarToggle", PERSISTENT},
+    {"MapboxToken", PERSISTENT},
 
     // Community
     {"SupportedCars", PERSISTENT},
