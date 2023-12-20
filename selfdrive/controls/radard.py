a6d9ae44223d4fa4318048429e5ef71ac901a56b
neokii <neokii@gmail.com>
2023-12-20 19:39:35 +0900

    update radar to camera

--- selfdrive/controls/radard.py
+++ selfdrive/controls/radard.py
@@ -24,7 +24,7 @@ SPEED, ACCEL = 0, 1     # Kalman filter states enum
 V_EGO_STATIONARY = 4.   # no stationary object flag below this speed
 
 RADAR_TO_CENTER = 2.7   # (deprecated) RADAR is ~ 2.7m ahead from center of car
-RADAR_TO_CAMERA = 2.2 #1.52  # RADAR is ~ 1.5m ahead from center of mesh frame
+RADAR_TO_CAMERA = 2.0 #1.52  # RADAR is ~ 1.5m ahead from center of mesh frame
 
 
 class KalmanParams:
