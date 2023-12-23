eec5b326d3e2036f0486be9ca107caa9fa070177
neokii <neokii@gmail.com>
2023-12-23 21:52:47 +0900

    fix

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -377,7 +377,7 @@ class CarStateBase(ABC):
 
   def update_clu_speed_kf(self, v_ego_raw):
     if abs(v_ego_raw - self.v_ego_clu_kf.x[0][0]) > 2.0:  # Prevent large accelerations when car starts at non zero speed
-      self.v_ego_clu_kf.x = [[v_ego_raw], [0.0]]
+      self.v_ego_clu_kf.set_x([[v_ego_raw], [0.0]])
 
     v_ego_x = self.v_ego_clu_kf.update(v_ego_raw)
     return float(v_ego_x[0]), float(v_ego_x[1])
